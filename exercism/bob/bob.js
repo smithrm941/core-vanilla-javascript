b57fc5a8d8ada5346fca2806e093297eb86d3721
smithrm941 <smithrm941@gmail.com>
2017-03-14 09:30:30 -0700

    completed bob, all tests passing

--- exercism/bob/bob.js
+++ exercism/bob/bob.js
@@ -4,20 +4,37 @@ He says 'Fine. Be that way!' if you address him without actually saying
 anything.
 He answers 'Whatever.' to anything else.*/
 
+// var Bob = function() {};
+//
+// Bob.prototype.hey = function( what ) {
+//   what.replace(/[^\A-Z]/gi, '')
+//   if ( what.endsWith('?')) {
+//     return 'Sure.';
+//   } else if (what == what.toUpperCase()){
+//     return 'Whoa, chill out!';
+//   } else if (what.trim() === ''){
+//     return 'Fine. Be that way!'
+//   }
+//   return 'Whatever.'
+//
+// }
+//
+// module.exports = Bob;
+
 var Bob = function() {};
 
 Bob.prototype.hey = function( what ) {
-  console.log(what);
-  what.replace(/[^\A-Z]/gi, '')
-  if ( what.charAt(what.length - 1) == '?' ) {
-    return 'Sure.';
-  } else if (what == what.toUpperCase()){
+  what.replace(/[^\A-Z0-9]/gi, '')
+  console.log(what.replace(/[^\A-Z0-9]/gi, ''));
+  if (what.match(/[A-Z]/) && what == what.toUpperCase()) {
     return 'Whoa, chill out!';
-  } else if (what == ''){
+  } else if (what.endsWith('?')) {
+    return 'Sure.';
+  } else if (what.trim() === ''){
     return 'Fine. Be that way!'
-  } else {
-    return 'Whatever.'
   }
-};
+  return 'Whatever.'
+
+}
 
 module.exports = Bob;
