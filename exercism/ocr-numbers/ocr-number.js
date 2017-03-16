f1d64e410d11101da02f66cd72d272b04e1fef68
Jacklyne Bouier <jacklynebouier@Jacklynes-MacBook-Pro.local>
2017-03-16 12:54:48 -0700

    ocr number passing all test except 1

--- exercism/ocr-numbers/ocr-number.js
+++ exercism/ocr-numbers/ocr-number.js
@@ -1,116 +1,125 @@
-var ocr function(){}
+var Ocr = function(){}
 
-ocr.prototype.convert(ocrStrings){
-  // var ocrString = (
-  //       '    _  _ \n' +
-  //       '  | _| _|\n' +
-  //       '  ||_  _|\n' +
-  //       '         \n'
-  //     );
-  var ocrString = (
-        '    _ \n' +
-        '  || |\n' +
-        '  ||_|\n' +
-        '      '
-      );
+  Ocr.prototype.convert = function(ocrString) {
+    var ocrSplit = ocrString.split('\n');
 
-
-
-  var ocrSplit = ocrString.split('\n');
-
-  var choppedOcr = [];
-  var ocrLoop = (function(){
-    for(i=0; i < ocrSplit.length; i++ ){
-      var nestedOcr = ocrSplit[i].match(/.{1,3}/g);
-      choppedOcr.push(nestedOcr);
-    }
-    return choppedOcr;
-
-
-  });
-  ocrLoop();
-
-
-
-  var ocrNumber = (function(){
-    var finalOcr = [];
-    for(var x=0; x < choppedOcr[x].length; x++){
-       for(var i=0; i < choppedOcr.length; i++){
-         finalOcr.push(choppedOcr[i][x]);
+    var choppedOcr = [];
+    var ocrLoop = (function(){
+      for(i=0; i < ocrSplit.length; i++ ){
+        var nestedOcr = ocrSplit[i].match(/.{1,3}/g);
+        choppedOcr.push(nestedOcr);
       }
+      return choppedOcr;
+    });
+    ocrLoop();
+
+    var finalOcr = [];
+    var ocrFinal = (function(){
+      for(var x=0; x < choppedOcr[0].length; x++){
+         for(var i=0; i < choppedOcr.length; i++){
+           finalOcr.push(choppedOcr[i][x]);
+        }
+      }
+      return finalOcr;
+    });
+    ocrFinal();
+
+
+    var convertedOcr = [];
+    var ocrMachine = function(){
+      while(finalOcr.length > 0){
+        var ocrNumber = finalOcr.slice(0,4).join('\n');
+          if(ocrNumber ==
+              ' _ \n' +
+              '| |\n' +
+              '|_|\n' +
+              '   '
+          ){
+         convertedOcr.push(0);
+
+          }else if(ocrNumber ==
+              '   \n' +
+              '  |\n' +
+              '  |\n' +
+              '   '
+            ){
+           convertedOcr.push(1);
+
+          }else if(ocrNumber ==
+              ' _ \n' +
+              ' _|\n' +
+              '|_ \n' +
+              '   '
+            ){
+          convertedOcr.push(2);
+          }else if(ocrNumber ==
+             ' _ \n' +
+              ' _|\n' +
+              ' _|\n' +
+              '   '
+            ){
+           convertedOcr.push(3);
+           }else if(ocrNumber ==
+              '   \n' +
+              '|_|\n' +
+              '  |\n' +
+              '   '
+            ){
+            convertedOcr.push(4);
+          } else if(ocrNumber ==
+              ' _ \n' +
+              '|_ \n' +
+              ' _|\n' +
+              '   '
+            ){
+            convertedOcr.push(5);
+          } else if(ocrNumber ==
+              ' _ \n' +
+              '|_ \n' +
+              '|_|\n' +
+              '   '
+          ){
+            convertedOcr.push(6);
+          } else if(ocrNumber ==
+              ' _ \n' +
+              '  |\n' +
+              '  |\n' +
+              '   '
+          ){
+            convertedOcr.push(7);
+          } else if(ocrNumber ==
+              ' _ \n' +
+              '|_|\n' +
+              '|_|\n' +
+              '   '
+          ){
+            convertedOcr.push(8);
+          } else if(ocrNumber ==
+              ' _ \n' +
+              '|_|\n' +
+              ' _|\n' +
+              '   '
+          ){
+            convertedOcr.push(9);
+          }else {
+            convertedOcr.push('?');
+          }
+
+          finalOcr.splice(0, 4);
+
+      }
+
+
+
     }
+        ocrMachine();
+        var string = convertedOcr.join('');
+        return string;
 
-      var ocrNumberOutput = finalOcr.join('\n');
-  console.log(ocrNumberOutput)
-  //     // console.log(ocrNumberOutput.substring(0,16));
-  //     // console.log(ocrNumberOutput.substring(16,31));
-  // var ocrNumberInputDivide = ocrNumberOutput.match(/.{1,16}/g);
-  //       console.log(ocrNumberInputDivide.join('\n'))
-  //       console.log(ocrNumberInputDivide)
-   });
-
-  // var ocrNumberInputDivide = (function(){
-  //   for(var c=0; c < ocrNumberOutput; c++ ){
-  //       ocrNumberOutput.substring(0,16)
-  //   }
-  // })
-
-
-  console.log(ocrNumber());
-
-  //       function ocrHell(ocrNumber){
-  //         var convertedOcr = [];
-  //       // if(ocrNumber.length < 16 || ocrNumber.length > 12){
-  //       //   return "Error";}else
-  //         if(ocrNumber ==
-  //         ' _ \n' +
-  //         '| |\n' +
-  //         '|_|\n'
-
-  //     ){
-  //     convertedOcr.push(0);
-
-  //     }else if(ocrNumber ==
-  //         '   \n' +
-  //         '  |\n' +
-  //         '  |\n'
-
-  //       ){
-  //     convertedOcr.push(1);
-
-  //     }else if(ocrNumber ==
-  //         ' _ \n' +
-  //         ' _|\n' +
-  //         '|_ \n'
-
-  //       ){
-  //     convertedOcr.push(2);
-  //     }
-
-  //     console.log(convertedOcr);
-  //   }
-
-
-
-
-  // var ocrInput = (
-  //       '    _ \n' +
-  //       '  || |\n' +
-  //       '  ||_|\n' +
-  //       '      '
-  //     );
-
-  //   console.log(ocrMachine(ocrInput));
+  }
 
 
 
 
 
-
-
-
-
-
-
-
-module.exports = ocr;
+module.exports = Ocr;
